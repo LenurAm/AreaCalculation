5239f7d4c2de15d6a8b88a955c7b4ca89dd0fed1
LenurAm <93513097+LenurAm@users.noreply.github.com>
2023-03-06 09:13:59 +0000

    Deploying to gh-pages from  @ f5dcd9e5f5ca6761d272540e46adaee55af4008f 🚀

---
+++ js/common.js
@@ -0,0 +1,26 @@
+﻿window.ShowToastr = (type, message) => { 
+    if (type === "success") {
+        toastr.success(message, "Operation success", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+window.SwalShow = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+        if (type === "error") {
+                    
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: message,
+                footer: '<a href="">Why do I have this issue?</a>'
+            })
+    }
+}       
